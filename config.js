c1b7d1cadf2eceb927a7fcf4548751a0b7ad05c9
webapp2024 <naiyachon@pornsirikul.ac.th>
2026-09-15 13:26:45 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -6,4 +6,4 @@
 const GAS_API_URL = "https://script.google.com/macros/s/AKfycbz76O7X4QDLflHrqvT8NPm3ALBuoAPz7Rzu9N9DbVuUlcWJTZGXSt6uWTviueAUXAzijw/exec";
 
 // LIFF ID (จาก LINE Developers > LIFF > สร้าง LIFF app ชี้มาที่หน้านี้)
-const LIFF_ID = "2010489525-upobwIM9";
+const LIFF_ID = "2010557528-if9M1oWy";
